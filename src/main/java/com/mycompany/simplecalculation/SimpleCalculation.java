685a2806cb04b02b47c790eff8de5e3790cd074b
ChristopherNydoo <st10538114@myemeris.edu.za>
2026-03-05 12:56:09 +0200

    add comments

--- src/main/java/com/mycompany/simplecalculation/SimpleCalculation.java
+++ src/main/java/com/mycompany/simplecalculation/SimpleCalculation.java
@@ -1,16 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.simplecalculation;
-
-/**
- *
- * @author lab_services_student
- */
+/*
+  ST10538114
+  Christopher Nydoo
+  Simple Calculation - Multiplies 2 numbers
+*/
 public class SimpleCalculation {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         System.out.println("Hello World!");
+        
     }
 }
