0af5652c91b79b5b724753103ec71efecfa1a054
ChristopherNydoo <st10538114@myemeris.edu.za>
2026-03-05 12:36:40 +0200

    initial commit

---
+++ src/main/java/com/mycompany/simplecalculation/SimpleCalculation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.simplecalculation;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class SimpleCalculation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
